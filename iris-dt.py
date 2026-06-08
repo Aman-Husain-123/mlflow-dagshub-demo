5961de6bab6606ffbe9ae335c5b225cfca7b66a3
Aman <amanhusain8899@gmail.com>
2026-06-08 21:14:22 +0530

    Dagshub repo connected

--- iris-dt.py
+++ iris-dt.py
@@ -6,11 +6,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import dagshub
-dagshub.init(repo_owner='campusx-official', repo_name='mlflow-dagshub-demo', mlflow=True)
+dagshub.init(repo_owner='Aman-Husain-123', repo_name='mlflow-dagshub-demo', mlflow=True)
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+mlflow.set_tracking_uri("https://dagshub.com/Aman-Husain-123/mlflow-dagshub-demo.mlflow")
 
 # Load the iris dataset
 iris = load_iris()
